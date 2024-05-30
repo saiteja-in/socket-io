3eff4e03e1a1c853beb91b2a13083d2c669dd5ed
saiteja-in <vurukondasaiteja13@gmail.com>
2024-05-31 01:14:48 +0530

    added dotenv

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express=require("express")
+require("dotenv").config();
 const { join } = require("path")
 const app=express()
 const {createServer}=require('http')
 const {Server}=require("socket.io")
 const server=createServer(app)
-
+const PORT=process.env.PORT || 3000
 const io=new Server(server)
 app.get("/",(req,res)=>{
     res.sendFile(join(__dirname,'index.html'))
